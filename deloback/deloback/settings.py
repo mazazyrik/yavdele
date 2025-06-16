a440eff2a46e96f30b9be1ca8d95307172d9169e
mazazyrik <117200062+mazazyrik@users.noreply.github.com>
2025-06-16 15:17:58 +0300

    set

--- deloback/deloback/settings.py
+++ deloback/deloback/settings.py
@@ -101,7 +101,7 @@ USE_TZ = True
 STATIC_URL = 'backend_static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
